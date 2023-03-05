b7988ccc044f0f51e91b33a2a26609216e1f2ca7
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-05 14:05:02 -0600

    added const sess in server.js file

--- server.js
+++ server.js
@@ -12,3 +12,18 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict'
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
