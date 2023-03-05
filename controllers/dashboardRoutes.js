e933df79688d94b0c794c260277260b417e37f2d
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-05 14:45:04 -0600

    added router.get code for dashboardRouter.js file

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -4,3 +4,22 @@ const sequelize = require('../config/connection');
 const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', withAuth, async (req, res) => {
+    try {
+        constpostData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id,
+            },
+            incluude: [User],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('dashboard', {
+            posts: posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
